497f62fd476159140f0c4aede72d9fa5ea1bf239
Şevval Alp <ssevvalalp@gmail.com>
2024-02-09 20:17:28 +0300

    HttpDelete Request

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -111,5 +111,34 @@ namespace ProductsAPI.Controllers
 
 
         }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteProduct(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(i => i.ProductId == id);
+            
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(Exception)
+            {
+                return NotFound();
+            }
+            return NoContent() ;
+        }
     }
 }
