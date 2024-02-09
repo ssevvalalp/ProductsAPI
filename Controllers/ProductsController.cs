a69b904079d56bd72a052334c7ad45d1ac7467b9
Şevval Alp <ssevvalalp@gmail.com>
2024-02-09 17:04:58 +0300

    Data Query

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProductsAPI.Models;
 
 namespace ProductsAPI.Controllers
@@ -7,35 +8,58 @@ namespace ProductsAPI.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private static List<Product>? _products;
-            
-        public ProductsController()
+
+        //private static List<Product>? _products;
+
+        //injection
+        private readonly ProductsContext _context;
+
+
+        public ProductsController(ProductsContext context)
         {
-            _products = new List<Product>();
-            _products.Add(new Product { ProductId = 1, ProductName = "iphone 14", Price = 12000, IsActive = true });
-            _products.Add(new Product { ProductId = 2, ProductName = "iphone 15", Price = 14000, IsActive = true });
+
+            _context = context;
+
         }
+
+        //public ProductsController()
+        //{
+
+        ////    _products = new List<Product>();
+        ////    {
+
+        ////        _products.Add(new Product { ProductId = 1, ProductName = "iphone 14", Price = 12000, IsActive = true });
+        ////        _products.Add(new Product { ProductId = 2, ProductName = "iphone 15", Price = 14000, IsActive = true });
+        ////    }
+
+        //}
 
 
         [HttpGet]
-        public IActionResult GetProducts()
+        public async Task <IActionResult> GetProducts()
         {
-           if (_products == null)
-            {
-                return NotFound();
-            }
-            return Ok( _products);
+           //if (_products == null)
+           // {
+           //     return NotFound();
+           // }
+           // return Ok( _products);
+
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
 
+
         [HttpGet("{id}")]
-        public IActionResult GetProducts(int? id)
+        public async Task <IActionResult> GetProducts(int? id)
         {
             if(id == null)
             {
                 return NotFound();
             }
 
-            var p = _products?.FirstOrDefault(i => i.ProductId == id);
+            //var p = _products?.FirstOrDefault(i => i.ProductId == id);
+
+            var p =await _context.Products.FirstOrDefaultAsync(i => i.ProductId == id);
 
             if (p == null)
             {
--- Models/ProductsContext.cs
+++ Models/ProductsContext.cs
@@ -4,10 +4,24 @@ namespace ProductsAPI.Models
 {
     public class ProductsContext:DbContext
     {
-        public DbSet<Product> Products { get; set; }
+        
         public ProductsContext(DbContextOptions<ProductsContext> options): base(options)
         {
            
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 1, ProductName = "iphone 14", Price = 12000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 2, ProductName = "iphone 15", Price = 14000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 3, ProductName = "iphone 16", Price = 18000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 4, ProductName = "iphone 17", Price = 20000, IsActive = false });
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
     }
 }
