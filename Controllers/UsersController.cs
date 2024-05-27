8b838482d42b4dcbd71e3f5ab3aa4313a8242fef
Şevval Alp <ssevvalalp@gmail.com>
2024-05-27 15:24:20 +0300

    Login

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -9,11 +9,14 @@ namespace ProductsAPI.Controllers
     [Route("api/[controller]")]
     public class UsersController:ControllerBase
     {
-        private UserManager<AppUser> _userManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public UsersController(UserManager<AppUser> userManager)
+        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+
         }
 
         [HttpPost("register")]
@@ -40,5 +43,26 @@ namespace ProductsAPI.Controllers
             return BadRequest(result.Errors);
 
         }
+
+        public async Task<IActionResult> Login(LoginDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                return BadRequest(new {message = "email hatalı"});
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if (result.Succeeded)
+            {
+                return Ok(new {token = "token"}
+                );
+            }
+
+            return Unauthorized();
+        }
+
     }
 }
---
+++ DTO/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsAPI.DTO
+{
+    public class LoginDTO
+    {
+        //kullanıcıya göndermek istenen alanlar
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
