07fc9fed300927bfd50d58eedd4991d05d118ebb
Şevval Alp <ssevvalalp@gmail.com>
2024-05-27 15:07:29 +0300

    Create User

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsAPI.DTO
+{
+    public class UserDTO
+    {
+        //kullanıcıya göndermek istenen alanlar
+
+        [Required]
+        public string FullName { get; set; } = null!;  
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
